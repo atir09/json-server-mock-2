4157f59cca7ef8b53ab9994cfd93d90199e5bbd2
Atir Khan <115460872+atir09@users.noreply.github.com>
2023-07-03 13:41:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const jsonserver=require("json-server")
-const cors=require ("cors")
+const cors=require("cors")
 
 
 const server=jsonserver.create()
 const router=jsonserver.router("db.json")
-const middlewares=jsonserver.defaults()
+// const middlewares=jsonserver.defaults()
 
 server.use(cors())
 server.use(jsonserver.bodyParser)
-server.use(middlewares)
+// server.use(middlewares)
 
-server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+// server.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     next();
+//   });
 
 server.use(router)
 
